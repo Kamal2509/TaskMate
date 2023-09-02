ad020f9d61eeceeb0347e26078218335513a9c74
Kamal Sharma <kamalsharma25091994@gmail.com>
2023-09-02 18:03:28 +0530

    final Teammate project code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import {Header} from "./components/Header";
 import {AddTask} from "./components/AddTask";
 import {ShowTask} from "./components/ShowTask";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
- const [tasklist, setTasklist]=useState([]);
+ const [tasklist, setTasklist]=useState(JSON.parse(localStorage.getItem("tasklist"))|| []);
  const [task, setTask]=useState({});
+
+useEffect(()=>{
+  localStorage.setItem("tasklist",JSON.stringify(tasklist))
+},[tasklist]);
+
   return (
     <div className='App' >
      <Header/> 
